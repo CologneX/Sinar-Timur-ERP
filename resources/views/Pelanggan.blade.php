993b1acc29f30cf33c24649e23d607272e853ef0
Kyrell <kleanosiauw@student.ciputra.ac.id>
2022-12-23 14:34:42 +0700

    hi

--- resources/views/Pelanggan.blade.php
+++ resources/views/Pelanggan.blade.php
@@ -3,6 +3,39 @@
     <div id="content">
         <div class="container-fluid">
             <h2 class="fw-bold text-dark mb-4" style="color: rgb(0,0,0);">Pelanggan</h2><!-- Start: #TabelBarang -->
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="container-fluid" style="padding-left: 0px;padding-right: 0px;">
+                        <div class="row g-0 d-flex d-lg-flex justify-content-sm-start align-items-lg-center"
+                            style="padding-bottom: 5px;">
+
+                            <div class="col d-sm-flex d-md-flex d-lg-flex d-xl-flex justify-content-end align-content-center justify-content-sm-end justify-content-md-end justify-content-xl-end"
+                                style="padding-left: 12px;">
+                                {{-- <button
+                                class="btn btn-outline-primary text-center d-flex justify-content-center align-items-center"
+                                type="button"
+                                style="margin-right: 9px;padding-right: 6px;padding-left: 4px;width: 84px;"
+                                data-bs-target="#tambah-barang" data-bs-toggle="offcanvas"><i
+                                    class="fas fa-plus"></i>&nbsp;Barang</button> --}}
+                                <div class="d-flex justify-content-end"><button
+                                        class="btn btn-outline-primary d-sm-flex justify-content-sm-end align-items-sm-center"
+                                        type="button" data-bs-toggle="modal" data-bs-target="#tambah-pelanggan"><i
+                                            class="fas fa-plus"></i>&nbsp;Tambah</button></div>
+                                {{-- <button
+                                class="btn btn-outline-primary d-flex d-sm-flex justify-content-center align-items-center justify-content-sm-end align-items-sm-center"
+                                type="button" data-bs-target="#filter-barang" data-bs-toggle="offcanvas"
+                                style="width: 84px;"><i class="fas fa-filter"></i>&nbsp;Filter</button> --}}
+                            </div>
+                        </div>
+                    </div>
+                    @livewire('tabel-pelanggan')
+                </div>
+            </div><!-- End: #TabelBarang -->
+        </div>
+    </div>
+    {{-- <div id="content">
+        <div class="container-fluid">
+            <h2 class="fw-bold text-dark mb-4" style="color: rgb(0,0,0);">Pelanggan</h2><!-- Start: #TabelBarang -->
             <!-- Start: #TabelPelanggan -->
             <div class="card shadow" id="tabel-pelanggan">
                 <div class="card-body">
@@ -14,7 +47,7 @@
                 </div>
             </div><!-- End: #TabelPelanggan -->
         </div>
-    </div>
+    </div> --}}
     <footer class="bg-white sticky-footer">
         <div class="container my-auto">
             <div class="text-center my-auto copyright"><span>Copyright Sinar Timur Sorong (C) 2022</span></div>
